d45a34eb4b639ac23327ae163c4fac7b3b8b9777
Art_Mary <artmary.ir@gmail.com>
2020-05-16 13:09:29 +0430

    add promisify.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 // create server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   // build file path dynamic
   let filePath = path.join(
     __dirname,
@@ -40,34 +43,68 @@ const server = http.createServer((req, res) => {
   // Check if contentType is text/html but no .html file extension
   if (contentType === 'text/html' && extname === '') filePath += '.html';
 
-  // Read file
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        // Page not found
-        fs.readFile(
-          path.join(__dirname, 'public', '404.html'),
-          // eslint-disable-next-line no-shadow
-          (err, content) => {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf8');
-          }
-        );
-      } else {
-        // server error
-        res.writeHead(500);
-        res.end('<h2> server error </h2>');
-      }
+  try {
+    const content = await readFile(filePath);
+    // success respond
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(content, 'utf8');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      // Page not found
+      fs.readFile(
+        path.join(__dirname, 'public', '404.html'),
+        // eslint-disable-next-line no-shadow
+        (err, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+        }
+      );
     } else {
-      // success respond
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf8');
+      // server error
+      res.writeHead(500);
+      res.end('<h2> server error </h2>');
     }
-  });
+  }
+
+  // Read file
+  // fs.readFile(filePath, (err, content) => {
+  //   if (err) {
+  //     if (err.code === 'ENOENT') {
+  //       // Page not found
+  //       fs.readFile(
+  //         path.join(__dirname, 'public', '404.html'),
+  //         // eslint-disable-next-line no-shadow
+  //         (err, content) => {
+  //           res.writeHead(404, { 'Content-Type': 'text/html' });
+  //           res.end(content, 'utf8');
+  //         }
+  //       );
+  //     } else {
+  //       // server error
+  //       res.writeHead(500);
+  //       res.end('<h2> server error </h2>');
+  //     }
+  //   } else {
+  //     // success respond
+  //     res.writeHead(200, { 'Content-Type': contentType });
+  //     res.end(content, 'utf8');
+  //   }
+  // });
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+
+(async () => {
+  await new Promise((res, rej) => {
+    server.listen(PORT, (e) => {
+      if (e) {
+        rej(e);
+      } else {
+        res();
+        console.log(`Server running on port: ${PORT}`);
+      }
+    });
+  });
+})();
+
